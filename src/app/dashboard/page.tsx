87af3bc2e28352ad793b0eef6e4f4768bc45bf82
joaovrcosta <joaovr.costa@gmail.com>
2025-02-12 11:57:43 -0300

    refactor: layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import Sidebar from "@/components/sidebar";
 import PinItem from "@/components/pin-item";
 import { Card } from "@/components/ui/card";
 import {
@@ -13,67 +12,62 @@ import { ProfitChart } from "@/components/charts/profit-chart";
 
 export default function DashboardPage() {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
-        <Sidebar />
-      </div>
-      <div className="flex flex-col lg:flex-row">
-        <main className="flex flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          <div className="flex items-center">
-            <h1 className="text-lg font-semibold md:text-2xl text-primary">
-              Pins
-            </h1>
-          </div>
-          <div className="space-y-3 max-w max-h-[820px] overflow-y-auto [&::-webkit-scrollbar]:hidden">
-            <PinItem name="João Victor" type="Pendencia" number="11999999999" />
-            <PinItem name="Maria" type="Pendencia" number="11999999999" />
-            <PinItem name="Guilherme" type="Pendencia" number="11999999999" />
-            <PinItem name="Elizel" type="Pendencia" number="11999999999" />
-            <PinItem name="Higor" type="Pendencia" number="11999999999" />
-          </div>
-        </main>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 lg:pl-0">
-          <div className="flex items-center">
-            <h1 className="text-lg font-semibold md:text-2xl text-primary">
-              Dashboard
-            </h1>
+    <div className="flex flex-col lg:flex-row">
+      <main className="flex flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        <div className="flex items-center">
+          <h1 className="text-lg font-semibold md:text-2xl text-primary">
+            Pins
+          </h1>
+        </div>
+        <div className="space-y-3 max-w max-h-[820px] overflow-y-auto [&::-webkit-scrollbar]:hidden">
+          <PinItem name="João Victor" type="Pendencia" number="11999999999" />
+          <PinItem name="Maria" type="Pendencia" number="11999999999" />
+          <PinItem name="Guilherme" type="Pendencia" number="11999999999" />
+          <PinItem name="Elizel" type="Pendencia" number="11999999999" />
+          <PinItem name="Higor" type="Pendencia" number="11999999999" />
+        </div>
+      </main>
+      <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 lg:pl-0">
+        <div className="flex items-center">
+          <h1 className="text-lg font-semibold md:text-2xl text-primary">
+            Dashboard
+          </h1>
+        </div>
+
+        <div>
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Mês" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">Dezembro</SelectItem>
+              <SelectItem value="dark">Novembro</SelectItem>
+              <SelectItem value="system">Outubro</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="w-full flex gap-4">
+            <Card className="w-full p-4">
+              <DefaultChartPayment />
+            </Card>
+
+            <Card className="w-full p-4">
+              <ProfitChart />
+            </Card>
           </div>
 
-          <div>
-            <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Mês" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="light">Dezembro</SelectItem>
-                <SelectItem value="dark">Novembro</SelectItem>
-                <SelectItem value="system">Outubro</SelectItem>
-              </SelectContent>
-            </Select>
+          <div className="w-full flex gap-4">
+            <Card className="w-full p-4">
+              <DefaultChartPayment />
+            </Card>
+
+            <Card className="w-full p-4">
+              <ProfitChart />
+            </Card>
           </div>
-          <div className="flex flex-col gap-4">
-            <div className="w-full flex gap-4">
-              <Card className="w-full p-4">
-                <DefaultChartPayment />
-              </Card>
-
-              <Card className="w-full p-4">
-                <ProfitChart />
-              </Card>
-            </div>
-
-            <div className="w-full flex gap-4">
-              <Card className="w-full p-4">
-                <DefaultChartPayment />
-              </Card>
-
-              <Card className="w-full p-4">
-                <ProfitChart />
-              </Card>
-            </div>
-          </div>
-        </main>
-      </div>
+        </div>
+      </main>
     </div>
   );
 }
--- src/app/students/page.tsx
+++ src/app/students/page.tsx
@@ -1,56 +1,51 @@
-import { Button } from '@/components/ui/button'
-import Sidebar from '@/components/sidebar'
-import { DataTable } from './data-table'
-import { columns, Payment } from './columns'
-import { Plus } from 'lucide-react'
-import SearchInput from '@/components/search-input'
-import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
-import { NewStudentModalContent } from '@/components/modals/new-student'
+import { Button } from "@/components/ui/button";
+import Sidebar from "@/components/sidebar";
+import { DataTable } from "./data-table";
+import { columns, Payment } from "./columns";
+import { Plus } from "lucide-react";
+import SearchInput from "@/components/search-input";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { NewStudentModalContent } from "@/components/modals/new-student";
 
 async function getData(): Promise<Payment[]> {
   return [
     {
-      id: '728ed52f',
+      id: "728ed52f",
       amount: 100,
-      status: 'pending',
-      email: 'm@example.com',
+      status: "pending",
+      email: "m@example.com",
     },
-  ]
+  ];
 }
 
 export default async function StudentsPage() {
-  const data = await getData()
+  const data = await getData();
 
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
-        <Sidebar />
-      </div>
-      <div className="flex flex-col">
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 ">
-          <div className="flex flex-col items-start justify-between">
-            <h1 className="text-lg font-semibold md:text-2xl mb-4 text-primary">
-              Alunos
-            </h1>
-            <div className="flex w-full space-x-3">
-              <SearchInput />
-              <Dialog>
-                <DialogTrigger asChild>
-                  <Button className="flex gap-3">
-                    Cadastrar aluno <Plus />
-                  </Button>
-                </DialogTrigger>
-                <DialogContent>
-                  <NewStudentModalContent />
-                </DialogContent>
-              </Dialog>
-            </div>
+    <div className="flex flex-col flex-1">
+      <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        <div className="flex flex-col items-start justify-between">
+          <h1 className="text-lg font-semibold md:text-2xl mb-4 text-primary">
+            Alunos
+          </h1>
+          <div className="flex w-full space-x-3">
+            <SearchInput />
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button className="flex gap-3">
+                  Cadastrar aluno <Plus />
+                </Button>
+              </DialogTrigger>
+              <DialogContent>
+                <NewStudentModalContent />
+              </DialogContent>
+            </Dialog>
           </div>
-          <section>
-            <DataTable columns={columns} data={data} />
-          </section>
-        </main>
-      </div>
+        </div>
+        <section>
+          <DataTable columns={columns} data={data} />
+        </section>
+      </main>
     </div>
-  )
+  );
 }
